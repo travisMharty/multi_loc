4c30914c3b03d0d7c6c6fb93e9e9135d8786b6c8
Travis Harty <harty@visit-a26.mmm.ucar.edu>
2018-06-12 12:42:22 -0600

    Added documentation to functions.

--- multi_loc/covariance.py
+++ multi_loc/covariance.py
@@ -4,6 +4,26 @@ import scipy.special
 
 
 def correlation_matern(rho, rho0, nu=2.5):
+    """
+    Return correlation as evaluated by the Matern correlation function
+
+    If nu = 0.5 then the Matern correlation function is equivalent to the
+    exponential covariance function.
+
+    As nu approaches infinity the Matern function approaches the squared
+    exponential covariance function.
+
+    Parameters
+    ----------
+    rho : array_like
+        Distances of first position to all others.
+
+    rho0 : Scalar
+        Characteristic distance.
+
+    nu : Scalar
+        Smoothness parameter. Must be positive real.
+    """
     rho1 = np.sqrt(2 * nu) * np.abs(rho) / rho0
     zero_indices = np.where(rho1 == 0)[0]
     corr = (rho1 ** nu
@@ -15,6 +35,20 @@ def correlation_matern(rho, rho0, nu=2.5):
 
 
 def correlation_exp(rho, rho0, nu=None):
+    """
+    Return correlation as evaluated by the exponential covariance function.
+
+    Parameters
+    ----------
+    rho : array_like
+        Distances of first position to all others.
+
+    rho0 : Scalar
+        Characteristic distance.
+
+    nu : Scalar
+        Unused dummy variable
+    """
     if nu is not None:
         print("nu is unused")
     corr = np.exp(-np.abs(rho/rho0))
@@ -22,6 +56,21 @@ def correlation_exp(rho, rho0, nu=None):
 
 
 def correlation_sqd_exp(rho, rho0, nu=None):
+    """
+    Return correlation as evaluated by the squared exponential covariance
+    function.
+
+    Parameters
+    ----------
+    rho : array_like
+        Distances of first position to all others.
+
+    rho0 : Scalar
+        Characteristic distance.
+
+    nu : Scalar
+        Unused dummy variable
+    """
     if nu is not None:
         print("nu is unused")
     corr = np.exp(-(rho**2 / (2 * rho0**2)))
@@ -29,6 +78,26 @@ def correlation_sqd_exp(rho, rho0, nu=None):
 
 
 def make_correlation_matrix(rho, rho0, correlation, nu=None):
+    """
+    Return correlation as evaluated by the squared exponential covariance
+    function.
+
+    Parameters
+    ----------
+    rho : array_like
+        Distances of first position to all others.
+
+    rho0 : Scalar
+        Characteristic distance.
+
+    correlation : function
+        Correlation function such that correlation(rho, rho0, nu) returns the
+        desired correlation for distances rho.
+
+    nu : Scalar
+        Matern smootheness parameter. Should be None is correlation is not
+        correlation_matern.
+    """
     rho_size = rho.size
     cor_vec = correlation(rho, rho0, nu)
     corr = np.zeros([rho_size, rho_size])
