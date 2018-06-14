5f915c7c173337b2b4dd991821492d8711f8b18e
Travis Harty <harty@visit-a26.mmm.ucar.edu>
2018-06-14 15:46:19 -0600

    Allow for generate_ensemble to take eig_val and eig_vec not P_sqrt.

--- multi_loc/covariance.py
+++ multi_loc/covariance.py
@@ -310,12 +310,15 @@ def matrix_sqrt_inv(C=None, eig_val=None, eig_vec=None, return_eig=False):
     return to_return
 
 
-def generate_ensemble(mu, P_sqrt, ens_size):
+def generate_ensemble(ens_size, mu, P_sqrt=None, eig_val=None, eig_vec=None):
     """
     Return ensemble drawn from N(mu, P).
 
     Parameters
     ----------
+    ens_size : scalar
+        Number of ensemble members to return.
+
     mu : array_like
         Mean of the returned ensemble samples. P.shape must equal
         (mu.size, mu.size).
@@ -323,8 +326,13 @@ def generate_ensemble(mu, P_sqrt, ens_size):
     P_sqrt : array_like
         Square root of the desired covariance of the ensemble samples.
 
-    ens_size : scalar
-        Number of ensemble members to return.
+    eig_val : array_like
+        Eigenvalues corresponding to the correlation matrix P.
+        If eig_val and eig_vec are provided, P_sqrt is not used.
+
+    eig_vec : array_like
+        Eigenvectors corresponding to the correlation matrix P.
+        If eig_val and eig_vec are provided, P_sqrt is not used.
 
     Returns
     ------
@@ -334,7 +342,18 @@ def generate_ensemble(mu, P_sqrt, ens_size):
     """
     dimension = mu.size
     mu = mu[:, None]
-    ens = mu + P_sqrt @ np.random.randn(dimension, ens_size)
+    eig_condition = ((eig_val is not None )
+                     and (eig_vec is not None))
+    if eig_condition:
+        eig_dim = eig_val.size
+        ens = (mu + eig_vec
+               @ (np.sqrt(eig_val[:, None])
+                  * np.random.randn(eig_dim, ens_size)))
+
+        # ens = (mu + (eig_vec @ np.diag(np.sqrt(eig_val)) @ eig_vec.conj().T)
+        #        @ np.random.randn(eig_dim, ens_size))
+    else:
+        ens = mu + P_sqrt @ np.random.randn(dimension, ens_size)
 
     return ens
 
